b903130885cc5a59671aab7a6fc37c1d4c558c1e
victor.macedo <526473+gb96@users.noreply.github.com>
2022-04-25 08:26:39 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank - 02/Program.cs
@@ -0,0 +1,32 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciarBonificacao gerenciador = new GerenciarBonificacao();
+            Funcionario carlos = new Funcionario();
+            carlos.Nome = "Carlos";
+            carlos.CPF = "546.879.157-20";
+            carlos.Salario = 2000;
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "454.658.148-30";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(carlos);
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine("Total de Bonificações: " + gerenciador.GetTotalBonificacao());
+            Console.ReadLine();
+        }
+    }
+}
